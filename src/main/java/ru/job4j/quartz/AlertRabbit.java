5f5b8b4edde2d34d830566895605fc484bc52607
sega <sergeykinn@mail.ru>
2020-11-15 14:17:23 +0300

    1. Quartz [#363341] исправления

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -7,6 +7,8 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -16,25 +18,27 @@ import static org.quartz.SimpleScheduleBuilder.*;
 public class AlertRabbit {
     public static void main(String[] args) throws IOException {
 
-        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        String appConfigPath = rootPath + "rabbit.properties";
-        Properties rabbitProps = new Properties();
-        rabbitProps.load(new FileInputStream(appConfigPath));
-        Integer interval = Integer.valueOf(rabbitProps.getProperty("rabbit.interval"));
-        try {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(interval)
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties rabbitProps = new Properties();
+            rabbitProps.load(in);
+            Integer interval = Integer.valueOf(rabbitProps.getProperty("rabbit.interval"));
+            try {
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                JobDetail job = newJob(Rabbit.class).build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(interval)
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+            } catch (SchedulerException se) {
+                se.printStackTrace();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
         }
     }
 
