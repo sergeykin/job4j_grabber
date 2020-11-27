ac621f1c971dc30c3bc9191a1197b29025f5de8a
sega <sergeykinn@mail.ru>
2020-11-27 17:31:36 +0300

    7. Web. [#363353] исправления

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -85,7 +85,8 @@ public class Grabber implements Grab {
                     try (OutputStream out = socket.getOutputStream()) {
                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         for (Post post : store.getAll()) {
-                            out.write(post.toString().getBytes());
+
+                            out.write(post.toString().getBytes("Windows-1251"));
                             out.write(System.lineSeparator().getBytes());
                         }
                     } catch (IOException io) {
